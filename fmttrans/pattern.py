5abb88bd0e41dc325b8f3740bc48580f3a21366d
logan <yuelong.chen.btr@gmail.com>
2020-01-08 18:42:40 +0800

    [A]: pattern.py for re compile

---
+++ fmttrans/pattern.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @License : Copyright(c) MIT License
+# @Time    : 8/1/2020 1:40 PM
+# @Author  : YUELONG.CHEN
+# @Mail    : yuelong.chen.btr@gmail.com
+# @File    : pattern
+# @Software: PyCharm
+
+import re
+
+cg_tag_pattern = re.compile('([0-9]+)([MNDI])')
